1a0fe309b2259e75ffec48304f220cb146760937
Danendra Clevero Ananda <danendraclever24@gmail.com>
2022-10-12 13:29:36 +0700

    roles

--- include/defender/defender.h
+++ include/defender/defender.h
@@ -7,4 +7,23 @@
 #include "multirole_func/multirole_func.h"
 #include "motion/motion.h"
 
+
+
+//=-------Global-------=//
+extern uint8_t game_status;
+extern uint8_t n_active_robot;
+static uint16_t game_sub_status;
+extern uint8_t n_active_robot;
+extern uint8_t style;
+
+//=--------Prep--------=//
+bool PreparationKickOffDef();
+
+//=--------Main--------=//
+void GameMainSingleDef();
+void GameMainDoubleDef();
+void GameMainTripleDef();
+void GameMainQuadrupleDef();
+
+
 #endif
--- src/defender.cpp
+++ src/defender.cpp
@@ -2,5 +2,142 @@
 
 void DefRun()
 {
-    printf("Hello def\n");
+    printf("=== DefRun ===\n");
+    // printf("Game status: %d\n", game_status);
+    switch (game_status)
+    {
+    case status_iddle_2:
+        motion_data.vel_x = 0;
+        motion_data.vel_y = 0;
+        motion_data.vel_th = 0;
+        motion_data.acceleration = 3;
+        ManualMotion(&motion_data, &motion_return);
+        break;
+
+    case keyboard_forward:
+        motion_data.vel_x = 0;
+        motion_data.vel_y = 10;
+        motion_data.vel_th = 0;
+        motion_data.acceleration = 3;
+        ManualMotion(&motion_data, &motion_return);
+        break;
+
+    case keyboard_backward:
+        motion_data.vel_x = 0;
+        motion_data.vel_y = -10;
+        motion_data.vel_th = 0;
+        motion_data.acceleration = 3;
+        ManualMotion(&motion_data, &motion_return);
+        break;
+
+    case keyboard_left:
+        motion_data.vel_x = -10;
+        motion_data.vel_y = 0;
+        motion_data.vel_th = 0;
+        motion_data.acceleration = 3;
+        ManualMotion(&motion_data, &motion_return);
+        break;
+
+    case keyboard_right:
+        motion_data.vel_x = 10;
+        motion_data.vel_y = 0;
+        motion_data.vel_th = 0;
+        motion_data.acceleration = 3;
+        ManualMotion(&motion_data, &motion_return);
+        break;
+
+    case keyboard_left_rotation:
+        motion_data.vel_x = 0;
+        motion_data.vel_y = 0;
+        motion_data.vel_th = -10;
+        motion_data.acceleration = 3;
+        ManualMotion(&motion_data, &motion_return);
+        break;
+
+    case keyboard_right_rotation:
+        motion_data.vel_x = 0;
+        motion_data.vel_y = 0;
+        motion_data.vel_th = 10;
+        motion_data.acceleration = 3;
+        ManualMotion(&motion_data, &motion_return);
+        break;
+
+    case status_preparation_kickoff_home:
+        PreparationKickOffDef();
+        break;
+
+    case game_kickoff_home:
+        printf("actibe robot: %d\n", n_active_robot);
+        if(n_active_robot > 4)
+            GameMainQuadrupleDef();
+        else if(n_active_robot > 3)
+            GameMainTripleDef();
+        else if(n_active_robot > 2)
+            GameMainDoubleDef();
+        else
+            GameMainSingleDef();
+        break;
+    }
+}
+
+bool PreparationKickOffDef()
+{
+    switch (game_sub_status)
+    {
+    case 0:
+        if (n_active_robot)
+            break;
+
+    case 1:
+        printf("aku def\n");
+        break;
+    }
+}
+
+void GameMainSingleDef()
+{
+    printf("%d %d\n", game_sub_status, style);
+   switch(game_sub_status)
+   {
+    case 0:
+        switch(style)
+        {
+            case 0:
+                game_sub_status = 10;
+                break;  
+            case 1:
+                game_sub_status = 20;
+                break;
+        }
+        break;
+    
+    case 10:
+        printf("masuk Def\n");
+        break;
+   }
+}
+
+void GameMainDoubleDef()
+{
+   switch(game_status)
+   {
+    case game_kickoff_home:
+        printf("masuk game kickoff home\n");
+        break;
+   }
+}
+
+void GameMainTripleDef()
+{
+   switch(game_status)
+   {
+    case game_kickoff_home:
+        printf("masuk game kickoff home\n");
+        break;
+   }
+}
+
+void GameMainQuadrupleDef()
+{
+   printf("Halo banh Quadruple\n");
 }
